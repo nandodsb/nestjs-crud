f2f64f2fb2b0d404af57a5712536629241fc6fbc
Fernando <nandosbx@users.noreply.github.com>
2021-11-20 19:42:34 -0300

    Update

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,10 +2,6 @@ namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
     PORT: string;
-    MONGO_URI: string;
-    db_user: string;
-    db_pass: string;
-    db_uri: string;
-    db_name: string;
+    MONGO_URI: string;    
   }
 }
