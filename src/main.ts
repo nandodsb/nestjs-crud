8bec1f98505586270f20b720692a679bcfef8c30
Fernando <nandosbx@users.noreply.github.com>
2021-11-21 02:22:38 -0300

    Eslint

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const devPort = 3000;
-  const devHost = "localhost";
+  const devHost = 'localhost';
 
   const serverPort = process.env.PORT || devPort;
-  const serverHost = "0.0.0.0" || devHost;
+  const serverHost = '0.0.0.0' || devHost;
 
   //await app.listen(process.env.PORT || 3000);
 
